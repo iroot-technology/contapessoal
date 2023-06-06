dffd2dcbd14eb3967903cf61d09e8a0f5d1f55ca
Emanuel A Carneiro <emanuel@timbira.com.br>
2023-06-06 20:55:55 -0300

    add databse file and update database start file

--- db/db.sql
+++ db/db.sql
@@ -21,3 +21,16 @@ create table stock_price (
 );
 
 alter table stock_price add constraint stock_price_fkey foreign key (id_stock) references stock(id);
+
+CREATE TYPE order_type AS ENUM ('B','S')
+
+create table order (
+    id serial primary key,
+    id_stock integer,
+    date date,
+    quantity integer,
+    price numeric(12,4),
+    type order_type,
+    created_at timestamp with time zone,
+);
+
